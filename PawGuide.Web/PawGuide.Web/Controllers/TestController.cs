e27627024d120213decf7ff5a529ae31edc876db
Dimitar <dimitar.nikolov.tvp@gmail.com>
2020-06-08 22:40:19 +0300

    Migrating to asp core 3.1

---
+++ PawGuide.Web/PawGuide.Web/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PawGuide.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Test()
+        {
+
+            return View();
+        }
+    }
+}
